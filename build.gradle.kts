6aa39f22969753ceb7afc3e2f66c7a6b58b3a014
Heiya <morooka.akira.inon29@gmail.com>
2023-02-07 08:16:26 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("MainKt")
+}
