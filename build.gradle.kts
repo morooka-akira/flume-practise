af1555797b8bbd800c5164e26d863222a47eaa3e
Heiya <morooka.akira.inon29@gmail.com>
2023-02-08 07:45:31 +0900

    skiaとLWJGLのimportとHelloworldの描画

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,37 @@
+import org.jetbrains.kotlin.ir.backend.js.compile
+
 plugins {
     kotlin("jvm") version "1.8.0"
     application
 }
 
-group = "org.example"
+group = "app.inon29"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
+/*************************** skiko ************************/
+val skVersion = "0.7.50"
+val target = "macos-arm64"
+
+/************************** lwjg ***************************/
+val lwjglVersion = "3.3.1"
+val lwjglNatives = "natives-macos-arm64"
+
 dependencies {
+    api("org.jetbrains.skiko:skiko-awt-runtime-$target:$skVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+
+    implementation("org.lwjgl:lwjgl:$lwjglVersion")
+    implementation("org.lwjgl:lwjgl-glfw:$lwjglVersion")
+    implementation("org.lwjgl:lwjgl-opengl:$lwjglVersion")
+    runtimeOnly("org.lwjgl:lwjgl:$lwjglVersion:$lwjglNatives")
+    runtimeOnly("org.lwjgl:lwjgl-glfw:$lwjglVersion:$lwjglNatives")
+    runtimeOnly("org.lwjgl:lwjgl-opengl:$lwjglVersion:$lwjglNatives")
+
     testImplementation(kotlin("test"))
 }
 
